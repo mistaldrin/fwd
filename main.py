b564340813be218db5d80c7ee384227c04c44f82
mistaldrin <mistaldrin@gmail.com>
2025-09-07 15:19:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from bot import Bot
+from database import initialize_database
 
 if __name__ == "__main__":
+    # First, initialize the database connection.
+    initialize_database()
+    
+    # Then, create and run the bot instance.
     app = Bot()
     app.run()
