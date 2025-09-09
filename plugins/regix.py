94f1be21f2ce23ded442f0e63e474ca6efbb7c5f
mistaldrin <mistaldrin@gmail.com>
2025-09-09 13:10:34 +0530

    Update regix.py

--- plugins/regix.py
+++ plugins/regix.py
@@ -76,7 +76,8 @@ async def pub_(bot, cb):
         if _bot.get('is_bot', False):
             start_point = min(i.start_id, i.end_id)
             end_point = max(i.start_id, i.end_id)
-            async for message in client.iter_messages(client, chat_id=i.FROM, limit=end_point, offset=start_point):
+            # The call is now corrected and does not pass the extra client argument.
+            async for message in client.iter_messages(chat_id=i.FROM, limit=end_point, offset=start_point):
                  if message: messages_to_process.append(message)
         else:
             start_point = max(i.start_id, i.end_id)
